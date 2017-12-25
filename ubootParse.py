1f4a7d3f45eda917dc5624d9d1d42722a3f3c201
[anp/hsw] <sysop@880.ru>
2017-12-25 13:26:28 +0700

    Support custom byte count in line, md.b/md.w friendly parser

--- ubootParse.py
+++ ubootParse.py
@@ -5,12 +5,14 @@ def main(argv):
 	inputfile = ' '
 	outputfile = ' '
 	debug = 0
+	strbytes = 16
 	try:
-		opts, args = getopt.getopt(argv, "dhi:o:",["ifile=","ofile="])
+		opts, args = getopt.getopt(argv, "dhi:o:b:",["ifile=","ofile="])
 	except getopt.GetoptError:
 		print "Invalid option, please run as:"
 		printhelp()
 		sys.exit(2)
+
 	for opt, arg in opts:
 		if opt == '-h':
 			printhelp()
@@ -21,12 +23,18 @@ def main(argv):
 			outputfile = arg
 		elif opt in ("-d"):
 			debug = 1
+		elif opt in ("-b"):
+			strbytes = int(arg)
+
+	if ((not inputfile) or (not inputfile) or (not opts)):
+		printhelp()
+		sys.exit(2)
 
 	print inputfile
 	print outputfile
-	timeToParse(inputfile, outputfile, debug)
+	timeToParse(inputfile, outputfile, debug, strbytes)
 
-def timeToParse(input, output, debug):
+def timeToParse(input, output, debug, strbytes):
 	f = open(input, 'r')
 	outfile = open(output, 'w')
 	lastoffset = 0
@@ -34,17 +42,17 @@ def timeToParse(input, output, debug):
 		line = f.readline()
 		if not line: break
 		offset = int(line[0:8], 16)
-		hextoParse = line[10:18] + line[19:27] + line[28:36] + line[37:45]
-		if ((lastoffset >= offset) or (lastoffset + 16 != offset)) and (lastoffset != 0):
-			print "Non-contigous offsets:"
+		strippedcontent = line[10:].replace(' ', '')
+		hextoParse = strippedcontent[0:strbytes*2]
+		if ((lastoffset >= offset) or (lastoffset + strbytes != offset)) and (lastoffset != 0):
+			print "Non-contigous offsets or incorrect number of bytes in source line:"
 			print "%08x" % lastoffset
 			print "%08x" % offset
 			sys.exit(2)
-		if len(hextoParse) != 32:
+		if len(hextoParse) != strbytes * 2:
 			print "Corrupted line:"
 			print line
 			sys.exit(2)
-		#hextoParse.replace(' ','')
 		if debug == 1:
 			print "%08x" % offset
 			print hextoParse
@@ -56,7 +64,7 @@ def timeToParse(input, output, debug):
 	print "Finished parsing, now exiting..."
 
 def printhelp():
-	print 'ubootParse.py -i <inputfile> -o <outputfile> -d'
+	print 'ubootParse.py -i <inputfile> -o <outputfile> -d -b [bytes in line]'
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
